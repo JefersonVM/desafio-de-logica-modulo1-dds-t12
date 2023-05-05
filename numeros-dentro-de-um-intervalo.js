b9d3e53b21e6664e8176a99ad8968a7018fc1e81
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-05-05 19:45:11 -0300

    Adicionando terceiro desafio

---
+++ numeros-dentro-de-um-intervalo.js
@@ -0,0 +1,15 @@
+
+const numero = 10;
+
+const limiteInferior = 5;
+
+const limiteSuperior = 20;
+
+if(numero >= limiteInferior && numero <= limiteSuperior){
+        
+    console.log(`PERTENCE`) 
+   
+   }else {
+       
+     console.log(`NAO PERTENCE`)  
+   }
