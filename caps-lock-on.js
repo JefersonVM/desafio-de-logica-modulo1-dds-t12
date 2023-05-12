644b81c40486c21a9e043e3c9b8b880c94a1e848
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-05-12 17:10:26 -0300

    Fazendo correção do exercício

--- caps-lock-on.js
+++ caps-lock-on.js
@@ -2,9 +2,9 @@
 
 const input = `cAPS`;
 
-if(input.substr(0, 1) === input.substr(0, 1).toLowerCase() && input.substr(1) === input.substr(1).toUpperCase()){
+if(input.substring(0, 1) === input.substring(0, 1).toLowerCase() && input.substring(1) === input.substring(1).toUpperCase()){
         
-    console.log(`${input.substr(0, 1).toUpperCase()}${input.substr(1).toLowerCase()}`);
+    console.log(`${input.substring(0, 1).toUpperCase()}${input.substring(1).toLowerCase()}`);
     
 }else if(input === input.toUpperCase()){
     
